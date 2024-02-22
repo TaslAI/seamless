a61c30489b0e6ffdac96f0d3e2fe785903f6ebf1
TaslAI <160872316+TaslAI@users.noreply.github.com>
2024-02-22 16:04:14 +0100

    Update taslFormParams.js

--- taslFormParams.js
+++ taslFormParams.js
@@ -1,7 +1,7 @@
 (function(){
 window.addEventListener('load', function(){
 
-	var targetControlId = 112037450; /** ID OF THE RECEIVER CONTROL **/
+	var targetControlId = "text-00000014"; /** ID OF THE RECEIVER CONTROL **/
 	var targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
 	targetControlInstance.setValue("hello");
