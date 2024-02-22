319655e757f4139763a4af6a6194f9f3269dfa12
TaslAI <160872316+TaslAI@users.noreply.github.com>
2024-02-22 16:52:47 +0100

    Update taslFormParams.js

--- taslFormParams.js
+++ taslFormParams.js
@@ -1,12 +1,18 @@
-(function(){
 
-	window.addEventListener('load', function()
-				{
-					document.getElementById("heading-0000000b-acc").innerHTML = "new title";
 
-	var targetControlId = "text-00000014"; /** ID OF THE RECEIVER CONTROL **/
-	var targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
-	targetControlInstance.setValue("hello");
-});
-})();
+
+window.document.onload = function(e){ 
+    setTimeout(() => {
+       let url = new URL(window.location.href);
+        document.getElementById("heading-0000000b-acc").innerHTML = url.searchParams.get('AutomationName');
+        document.getElementById("heading-0000000d-acc").innerHTML = url.searchParams.get('AutomationSummary');
+      }, "500");
+   
+}
+
+setTimeout(() => {
+    let url2 = new URL(window.location.href);
+    document.getElementById("heading-0000000b-acc").innerHTML = url2.searchParams.get('AutomationName');
+    document.getElementById("heading-0000000d-acc").innerHTML = url2.searchParams.get('AutomationSummary');
+  }, "500");
