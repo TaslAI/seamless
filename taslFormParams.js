bfaae85abfcfe18769240c179e2739c53c4940b9
TaslAI <160872316+TaslAI@users.noreply.github.com>
2024-02-22 15:31:04 +0100

    Update taslFormParams.js

--- taslFormParams.js
+++ taslFormParams.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    var labelElement = document.getElementById('112036845');
-    if (labelElement) {
-      labelElement.textContent = 'Your New Text Here';
-    }
-  }, 5000); // Adjust the delay as needed, here it's set to 5000 milliseconds (5 seconds)
-});
+setInterval(function() {
+  var labelElement = document.getElementById('112036845');
+  if (labelElement && labelElement.textContent === "AutomationSummaryPlaceholder") {
+    labelElement.textContent = 'Your New Text Here';
+    // Optionally clear the interval if you're sure it only needs to run once
+    // clearInterval(thisInterval);
+  }
+}, 1000); // Checks every 1000 milliseconds (1 second)
